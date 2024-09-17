e282d9b81819f0042b515c2d6dda5dbd7e438cff
wsmh <1362384538@qq.com>
2024-09-17 09:27:45 +0800

    完成侧边栏点击后Icon的外观响应,修整了一些css文件

--- script/sideBar.js
+++ script/sideBar.js
@@ -1,11 +1,12 @@
+import { icons } from "../data/sideBarActiveIcons.js";
 const sideBar_item = document.querySelectorAll('.sideBar-item');
 sideBar_item.forEach((item) => {
     item.addEventListener('click', () => {
         sideBar_item.forEach((i) => {
             i.classList.remove('sideBar-active');
+            i.firstElementChild.style.backgroundPositionX = icons[i.dataset.iconName] + 'px';
         })
-        console.log(item.firstChild);
-
+        item.firstElementChild.style.backgroundPositionX = icons[item.dataset.iconName + '-a'] + 'px';       //应为firstElementChild而非firstChild
         item.classList.add('sideBar-active');
     })
 })
